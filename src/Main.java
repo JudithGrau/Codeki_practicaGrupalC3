a3ad9d625f6075007843c65c347a88c6a2aec561
Bianca Stork <biancastrk@gmail.com>
2024-02-19 18:10:33 -0300

    Codeki_PracticaGrupalC3: se agrego Bianca al saludo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Grupo 2!");
+        System.out.println("Hello Grupo 2! - I'm Bianca :)");
     }
 }
